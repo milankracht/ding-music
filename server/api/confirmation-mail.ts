ba5c811ee0d960a2ec8e5a674d216b9035fa293e
Milan Kracht <milan@domini.nl>
2025-10-20 19:44:55 +0200

    Nodemailer enabled again

--- server/api/confirmation-mail.ts
+++ server/api/confirmation-mail.ts
@@ -4,30 +4,30 @@
 
 import nodemailer from 'nodemailer'
 
-// export default defineEventHandler(async (event) => {
-//   const body = await readBody(event)
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event)
 
-//   const transporter = nodemailer.createTransport({
-//     host: 'ding-music.nl',
-//     port: 465,
-//     secure: true,
-//     auth: {
-//       user: 'info@ding-music.nl',
-//       pass: 'Wnur77%80',
-//     },
-//   })
+  const transporter = nodemailer.createTransport({
+    host: 'ding-music.nl',
+    port: 465,
+    secure: true,
+    auth: {
+      user: 'mail@ding-music.nl',
+      pass: process.env.MAIL_PASSWORD,
+    },
+  })
 
-//   await transporter.sendMail({
-//     from: '"DING!" <info@ding-music.nl>',
-//     to: body.to,
-//     bcc: 'milankr8@icloud.com',
-//     subject: body.subject,
-//     text: body.message,
-//     html: `<p>${body.message}</p>`,
-//   })
+  await transporter.sendMail({
+    from: '"DING!" <info@ding-music.nl>',
+    to: body.to,
+    bcc: 'milankr8@icloud.com',
+    subject: body.subject,
+    text: body.message,
+    html: `<p>${body.message}</p>`,
+  })
 
-//   return { success: true }
-// })
+  return { success: true }
+})
 
 /**
  * Sends a confirmation email using the Resend API.
